ca7a941b0f8e2c443b9d4939bbbae7c6da50d68a
safyanansari <124531876+safyanansari@users.noreply.github.com>
2025-08-11 23:07:11 +0530

    array_pgms

---
+++ array programs/reverse.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+class Reverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = scanner.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+                for (int i = 0; i < rows; i++) {
+            int start = 0;
+            int end = cols - 1;
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+                System.out.println("Reversed matrix (row-wise):");
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+
+        
+    }
+}
